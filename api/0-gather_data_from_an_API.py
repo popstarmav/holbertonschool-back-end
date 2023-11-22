2a94cecfc7d42be27ee6222d49c1b6615143b04c
popstarmav <cj.johnson2017@gmail.com>
2023-11-21 18:29:59 -0800

    Gather data from an API

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -3,32 +3,30 @@
 place holder
 """
 
-if __name__ == "__main__":
+import requests
+from sys import argv
 
-    import requests
-    from sys import argv
+if __name__ == "__main__":
     if len(argv) < 2:
         exit()
 
     user_id = argv[1]
 
+    # Fetch all tasks for the user
     todos_response = requests.get(
-        f"https://jsonplaceholder.typicode.com/todos?"
-        f"userId={user_id}&completed=true"
+        f"https://jsonplaceholder.typicode.com/todos?userId={user_id}"
     )
     name_response = requests.get(
         f"https://jsonplaceholder.typicode.com/users?id={user_id}"
     )
-    todos_data = todos_response.json()
-    name_data = name_response.json()
 
+    name_data = name_response.json()
     name = name_data[0]["name"] if name_data else None
+
+    todos_data = todos_response.json()
     total_tasks = len(todos_data)
 
-    todo_list = []
-
-    for task in todos_data:
-        todo_list.append(f"\t{task['title']}")
+    todo_list = [f"\t{task['title']}" for task in todos_data]
 
     print(f"Employee {name} is done with tasks({total_tasks}/{total_tasks}):")
 
